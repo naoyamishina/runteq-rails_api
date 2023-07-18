9c81e3b997d8b842508ab0ec55b4f4204b21e58f
mishina naoya <msnyxx811@gmail.com>
2023-07-19 07:35:01 +0900

    fin

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V1
     class BaseController < ApplicationController
+      include Api::ExceptionHandler
+
     end
   end
 end
--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe 'Api::V1::Articles', type: :request do
     end
 
     it 'returns articles in json format' do
-      get api_v1_articles_path, headers: { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', }
+      get api_v1_articles_path, headers: { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' }
 
-      expect(JSON.parse(body)['data'].count).to eq(article_num)
+      expect(body['data'].count).to eq(article_num)
       expect(response).to be_successful
       expect(response).to have_http_status(:ok)
     end
@@ -21,10 +21,10 @@ RSpec.describe 'Api::V1::Articles', type: :request do
     let!(:article) { create(:article, user: user) }
 
     it 'returns articles in json format' do
-      get api_v1_article_path(article), headers: { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', }
+      get api_v1_article_path(article), headers: { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' }
 
-      expect(JSON.parse(body)['data']['id'].to_i).to eq(article.id)
-      expect(JSON.parse(body).dig('data', 'relationships', 'user', 'data', 'id').to_i).to eq(user.id)
+      expect(body['data']['id'].to_i).to eq(article.id)
+      expect(body.dig('data', 'relationships', 'user', 'data', 'id').to_i).to eq(user.id)
       expect(response).to be_successful
       expect(response).to have_http_status(:ok)
     end
