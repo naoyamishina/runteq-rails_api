37b9645c33e5484bf105b536488eeaea8cda2da0
mishina naoya <msnyxx811@gmail.com>
2023-07-18 23:02:35 +0900

    fin

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,11 +3,26 @@
 module Api
   module V1
     class ArticlesController < BaseController
+      before_action :set_article, only: :show
+      
       def index
         articles = Article.all
         json_string = ArticleSerializer.new(articles).serialized_json
 
         render json: json_string
+      end
+
+      def show
+        options = { include: [:user, :'user.name', :'user.email'] }
+        json_string = ArticleSerializer.new(@article, options).serialized_json
+
+        render json: json_string
+      end
+
+      private
+
+      def set_article
+        @article = Article.find(params[:id])
       end
     end
   end
