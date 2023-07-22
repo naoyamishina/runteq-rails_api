2608b1896194d1a295cfcba960433d7dfef8ded7
mishina naoya <msnyxx811@gmail.com>
2023-07-22 19:08:38 +0900

    fin

--- app/controllers/api/v1/user/articles_controller.rb
+++ app/controllers/api/v1/user/articles_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class User::ArticlesController < BaseController
       before_action :set_articles, only: :index
-      before_action :set_article, only: %i[show update destroy]
+      before_action :set_article, only: %i[update destroy]
 
       def index
         json_string = ArticleSerializer.new(@articles).serialized_json
